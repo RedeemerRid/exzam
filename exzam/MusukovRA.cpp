a024c2c696dba8bd7cc487d47c70dee099982492
RedeemerRid <95246748+RedeemerRid@users.noreply.github.com>
2022-08-13 10:46:11 +0300

    Update MusukovRA.cpp

--- exzam/MusukovRA.cpp
+++ exzam/MusukovRA.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>  ///    MUSUKOV ----------
 #include <iomanip>
 #include <io.h>
@@ -12,15 +14,15 @@ struct workdate {
 	int year;
 };
 
-struct worker{
+struct worker {
 	char name[10];
 	char surename[10];
-	char post [10];
+	char post[10];
 	workdate date;
 };
 
 
-void addWorker(worker *a, int& m, const int n)
+void addWorker(worker* a, int& m, const int n)
 {
 	if (m < n)
 	{
@@ -43,23 +45,21 @@ void addWorker(worker *a, int& m, const int n)
 	}
 	else
 	{
-		cout << "Размер превышен";
-		system("pause");
+		cout << "Warning bufer UPS";
 		cout << endl;
+		system("pause");
+		
 	}
-	
-	
+
+
 }
 
 
-
-
-
-void printWorker(worker *a, int& m) {
+void printWorker(worker* a, int& m) {
 	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
 	for (int i = 0; i < m; i++)
 	{
-		cout << setw(15) << left << setw(15) << a[i].name << setw(15) << a[i].surename << setw(15) << a[i].post << a[i].date.day <<"."<< a[i].date.month <<"."<< a[i].date.year << endl;
+		cout << setw(15) << left << setw(15) << a[i].name << setw(15) << a[i].surename << setw(15) << a[i].post << a[i].date.day << "." << a[i].date.month << "." << a[i].date.year << endl;
 	}
 	system("pause");
 }
@@ -82,12 +82,8 @@ void delWorker(worker* a, int& m) {
 			}
 			m--;
 			i--;
-			
 		}
-
 	}
-	
-
 }
 
 
@@ -126,73 +122,142 @@ void savedata_bin(worker* arr, int& size)
 
 
 // SOKUROVA---------------------------------
-void search1(worker *a, int& m) {
-	cout << "Поиск по имени" << endl;
-	char name[10];
-	cin >> name;
-	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
-	for (int i = 0;i < m;i++) {
-		if (strcmp(name, a[i].name) == 0)
-			cout << setw(15) << left << setw(15) << a[i].name << setw(15) << a[i].surename << setw(15) << a[i].post << a[i].date.day << "." << a[i].date.month << "." << a[i].date.year << endl;
-	}
-	system("pause");
-}
-void search2(worker*a, int& m) {
-	cout << "Поиск по Фамилий" << endl;
-	char famely[10];
-	cin >> famely;
-	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
-	for (int i = 0;i < m;i++) {
-		if (strcmp(famely, a[i].surename) == 0)
-			cout << setw(15) << left << setw(15) << a[i].name << setw(15) << a[i].surename << setw(15) << a[i].post << a[i].date.day << "." << a[i].date.month << "." << a[i].date.year << endl;
-	}
-	system("pause");
-}
-void search3(worker*a, int& m) {
-	cout << "Поиск по Должности" << endl;
-	char post[10];
-	cin >> post;
-	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
-	for (int i = 0;i < m;i++) {
-		if (strcmp(post, a[i].post) == 0)
-			cout << setw(15) << left << setw(15) << a[i].name << setw(15) << a[i].surename << setw(15) << a[i].post << a[i].date.day << "." << a[i].date.month << "." << a[i].date.year << endl;
-	}
-	system("pause");
-}
-
-void sortWorker(worker* a, int& m) {
-	
 
 
-		for (int i = 0; i < m - 1; i++)
-		{
-			for (int j = i + 1; j < m; j++)
+
+worker *sortWorker(worker* a, int& m) {
+
+	worker* b = new worker[m];
+	for (int i = 0; i < m; i++)
+	{
+		b[i] = a[i];
+	}
+			for (int i = 0; i < m; i++)
 			{
-				if (a[i].date.year > a[j].date.year)
+				for (int j = 0; j < m; j++)
 				{
-					int tmp = a[i].date.year;
-					a[i].date.year = a[j].date.year;
-					a[j].date.year = tmp;
+					if (b[i].date.year > b[j].date.year)
+					{
+						worker tmp = b[i];
+						b[i] = b[j];
+						b[j] = tmp;
+					}
 				}
 			}
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					if (b[i].date.year == b[j].date.year)
+					{
+						if (b[i].date.month > b[j].date.month)
+						{
+							worker tmp = b[i];
+							b[i] = b[j];
+							b[j] = tmp;
+						}
+					}
+					
+				}
+			}
+			
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					if (b[i].date.year == b[j].date.year && b[i].date.month == b[j].date.month)
+					{
+						if (b[i].date.day > b[j].date.day)
+						{
+							worker tmp = b[i];
+							b[i] = b[j];
+							b[j] = tmp;
+						}
+					}
 
-		}
+				}
+			}
+			/*
+			cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
+			for (int i = 0; i < m; i++) {
+				cout << setw(15) << left << setw(15) << b[i].name << setw(15) << b[i].surename << setw(15) << b[i].post << b[i].date.day << "." << b[i].date.month << "." << b[i].date.year << endl;
+			}
+			system("pause");
+			*/
+			return b;
 }
 
+void printSort(worker *a, int& m)
+{
+	worker* b;
+	b = sortWorker(a, m);
+	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
+	for (int i = 0; i < m; i++) {
+		cout << setw(15) << left << setw(15) << b[i].name << setw(15) << b[i].surename << setw(15) << b[i].post << b[i].date.day << "." << b[i].date.month << "." << b[i].date.year << endl;
+	}
+	system("pause");
+}
+
+void search1(worker* a, int& m) {
+	cout << "find by name :" << endl;
+	char name[10];
+	worker* b;
+	
+	cin >> name;
+	b = sortWorker(a, m);
+	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
+	for (int i = 0; i < m; i++) {
+		if (strcmp(name, b[i].name) == 0)
+			
+			cout << setw(15) << left << setw(15) << b[i].name << setw(15) << b[i].surename << setw(15) << b[i].post << b[i].date.day << "." << b[i].date.month << "." << b[i].date.year << endl;
+	}
+	system("pause");
+}
+void search2(worker* a, int& m) {
+	cout << "find by surname" << endl;
+	char famely[10];
+	worker* b;
+	cin >> famely;
+	b = sortWorker(a, m);
+	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
+	for (int i = 0; i < m; i++) {
+		if (strcmp(famely, b[i].surename) == 0)
+			
+			cout << setw(15) << left << setw(15) << b[i].name << setw(15) << b[i].surename << setw(15) << b[i].post << b[i].date.day << "." << b[i].date.month << "." << b[i].date.year << endl;
+	}
+	system("pause");
+}
+void search3(worker* a, int& m) {
+	cout << "find by post" << endl;
+	char post[10];
+	cin >> post;
+	worker* b;
+	b = sortWorker(a, m);
+	cout << setw(15) << left << setw(15) << "NAME" << setw(15) << "SURNAME" << setw(15) << "POST" << setw(15) << "DATE" << endl;
+	for (int i = 0; i < m; i++) {
+		if (strcmp(post, b[i].post) == 0)
+			
+			cout << setw(15) << left << setw(15) << b[i].name << setw(15) << b[i].surename << setw(15) << b[i].post << b[i].date.day << "." << b[i].date.month << "." << b[i].date.year << endl;
+	}
+	system("pause");
+}
+
+
+
 // SOKUROVA-----------------------------
- 
- 
+
+
 
 // MUSUKOV ----------------------
 int main()
 {
-	setlocale(LC_ALL, "rus");
+	//setlocale(LC_ALL, "rus");
 	bool exit = true;
-	const int n = 3;
+	const int n = 10;
 	int m = 0;
 	worker a[n];
 	int x;
-	
+
 	do
 	{
 		cout << "1 - print" << endl;
@@ -204,12 +269,12 @@ int main()
 		cout << "7 - find by fname" << endl;
 		cout << "8 - find by date" << endl;
 		cout << "9 - sort by year" << endl;
-		
+
 		cout << "0 - quit" << endl;
 
 		cout << "     chooose position : ";
 
-		while (!isdigit(char(cin.peek())))                      //  проверка. когда вместо int вводишь что то другоу, игнорирует ввод
+		while (!isdigit(char(cin.peek())))                      
 		{
 			system("cls");
 			cin.ignore();       //-----------------------------------------------------
@@ -262,9 +327,9 @@ int main()
 			search3(a, m);
 			break;
 		case 9://2
-			sortWorker(a, m);
+			printSort(a, m);
 			break;
-		
+
 
 		}
 	} while (exit != 0);
